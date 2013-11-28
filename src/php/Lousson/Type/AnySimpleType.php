ad45e22d9602cc4569b87aefa981218158e71e10
Mathias J. Hennig <mathias.hennig@sedo.com>
2013-11-28 14:39:18 +0100

    Initial commit

---
+++ src/php/Lousson/Type/AnySimpleType.php
@@ -0,0 +1,151 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=75: *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2013, The Lousson Project                               *
+ *                                                                       *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * Redistribution and use in source and binary forms, with or without    *
+ * modification, are permitted provided that the following conditions    *
+ * are met:                                                              *
+ *                                                                       *
+ * 1) Redistributions of source code must retain the above copyright     *
+ *    notice, this list of conditions and the following disclaimer.      *
+ * 2) Redistributions in binary form must reproduce the above copyright  *
+ *    notice, this list of conditions and the following disclaimer in    *
+ *    the documentation and/or other materials provided with the         *
+ *    distribution.                                                      *
+ *                                                                       *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,            *
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)    *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,   *
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)         *
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED   *
+ * OF THE POSSIBILITY OF SUCH DAMAGE.                                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ * Lousson\Type\AnySimpleType interface definition
+ *
+ * @package     org.lousson.type
+ * @copyright   (c) 2013, The Lousson Project
+ * @license     http://opensource.org/licenses/bsd-license.php New BSD License
+ * @author      Mathias J. Hennig <mhennig at quirkies.org>
+ * @filesource
+ */
+namespace Lousson\Type;
+
+/* Interfaces: */
+use Lousson\Type\AnyType;
+
+/**
+ * An interface for simple types
+ *
+ * @since       lousson/Lousson_Type-0.1.0
+ * @package     org.lousson.type
+ */
+interface AnySimpleType extends AnyType
+{
+    /**
+     * Import from lexical to value space
+     *
+     * The import() method converts the given $lexical representation to
+     * it's internal value.
+     *
+     * @param   string  $lexical    The string to import
+     *
+     * @return  mixed
+     *          The internal representation is returned on success
+     *
+     * @throws  \Lousson\Type\AnyTypeException
+     *          All possible exceptions implement this interface
+     *
+     * @throws  \InvalidArgumentException
+     *          Raised in case either the given $lexical representation is
+     *          or the imported value would be considered invalid
+     *
+     * @throws  \RuntimeException
+     *          Raised in case of an internal error
+     */
+    public function import($lexical);
+
+    /**
+     * Export from value to lexical space
+     *
+     * The export() method is used to convert the given $value (which
+     * must be a compatible, probably import()ed representation) to it's
+     * canonical lexical representation.
+     *
+     * @param   mixed   $value      The value to export
+     *
+     * @return  string
+     *          The $value's canonical representation is returned on success
+     *
+     * @throws  \Lousson\Type\AnyTypeException
+     *          All possible exceptions implement this interface
+     *
+     * @throws  \InvalidArgumentException
+     *          Raised in case the given $value is considered invalid
+     *
+     * @throws  \RuntimeException
+     *          Raised in case of an internal error
+     */
+    public function export($value);
+
+    /**
+     * Import a value from another type's lexical space
+     *
+     * The importFrom() method is used to import the given $lexical from
+     * the $type's lexical space into the own value space.
+     *
+     * @param   AnySimpleType   $type       The type to import from
+     * @param   string          $lexical    The value to import
+     *
+     * @return  mixed
+     *          The imported value is returned on success
+     *
+     * @throws  \Lousson\Type\AnyTypeException
+     *          All possible exceptions implement this interface
+     *
+     * @throws  \InvalidArgumentException
+     *          Raised in case either the given $lexical representation is
+     *          or the imported value would be considered invalid by either
+     *          of the two types
+     *
+     * @throws  \RuntimeException
+     *          Raised in case of an internal error
+     */
+    public function importFrom(AnySimpleType $type, $lexical);
+
+    /**
+     * Export a value into another type's lexical space
+     *
+     * The exportTo() method is used to export the given $value from the
+     * own value space into the lexical space of the given $type.
+     *
+     * @param   AnySimpleType   $type   The type to export to
+     * @param   mixed           $value  The value to export
+     *
+     * @return  string
+     *          The exported representation is returned on success
+     *
+     * @throws  \Lousson\Type\AnyTypeException
+     *          All possible exceptions implement this interface
+     *
+     * @throws  \InvalidArgumentException
+     *          Raised in case the given $value is considered invalid by
+     *          either of the two types
+     *
+     * @throws  \RuntimeException
+     *          Raised in case of an internal error
+     */
+    public function exportTo(AnySimpleType $type, $value);
+}
+
